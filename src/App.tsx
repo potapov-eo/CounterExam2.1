92cb381e21d3fafcf6a1474c9b3a71133b08513a
potapov-eo <potapov.eo@yandex.ru>
2020-11-11 00:08:06 +0300

    Counter 2.1 finish + localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,22 @@ import Counter from "./Counter";
 import SetValueForCounter from "./SetValueForCounter";
 
 function App() {
+    let min = Number(localStorage.getItem("minCount")) ?
+        Number(localStorage.getItem("minCount"))
+        : 5
+    let max = Number(localStorage.getItem("maxCount")) ?
+        Number(localStorage.getItem("maxCount"))
+        : 10
 
-    const [maxCount, setMaxCount] = useState<number>(10)
-    const [minCount, setMinCount] = useState<number>(5)
+    const [maxCount, setMaxCount] = useState<number>(max)
+    const [minCount, setMinCount] = useState<number>(min)
     const [showCounter, setShowCounter] = useState<boolean>(false)
 
     let changeCountInitState = (timeMaxCount: number, timeMinCount: number) => {
         setMaxCount(timeMaxCount)
+        localStorage.setItem("maxCount", timeMaxCount.toString())
         setMinCount(timeMinCount)
+        localStorage.setItem("minCount", timeMinCount.toString())
         setShowCounter(true)
     }
     let changeShowCountInit = (x: boolean) => {
